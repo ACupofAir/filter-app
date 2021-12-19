8831aa8325530e277cc4b00344e16117ab7a0354
ACupofAir <timethoughts@outlook.com>
2021-12-20 04:46:39 +0800

    add some img & fix the bug of linker

--- gui/linkers/linker.js
+++ gui/linkers/linker.js
@@ -1,130 +1,136 @@
-var path = require("path")
+var path = require("path");
 
 function change_to_kuw() {
-  document.getElementById('filter_name').innerText = 'Kuwa'
+  document.getElementById("filter_name").innerText = "Kuwa";
 }
 
 function change_to_glg() {
-  document.getElementById('filter_name').innerText = 'glg'
+  document.getElementById("filter_name").innerText = "glg";
 }
 
 function change_to_mean() {
-  document.getElementById('filter_name').innerText = 'mean'
+  document.getElementById("filter_name").innerText = "mean";
 }
 
 function filter_img() {
-  if (document.getElementById('filter_name').innerText == 'Kuwa') {
-    filter_with_kuw()
-  } else if (document.getElementById('filter_name').innerText == 'glg') {
-    filter_with_glg()
-  } else if (document.getElementById('filter_name').innerText == 'mean') {
-    filter_with_mean()
+  if (document.getElementById("filter_name").innerText == "Kuwa") {
+    filter_with_kuw();
+  } else if (document.getElementById("filter_name").innerText == "glg") {
+    filter_with_glg();
+  } else if (document.getElementById("filter_name").innerText == "mean") {
+    filter_with_mean();
   }
 
-
-  // Show preview of origin img
-  let img_url = path.join(__dirname, 'images', imgname)
-  document.getElementById('img-prev-div').setAttribute('src', img_url)
   function filter_with_kuw() {
     function filter() {
-      var imgname = document.getElementById("imgname").value
-
+      var imgName = document.getElementById("imgname").value;
+      // Show preview of origin img
+      let img_url = path.join(__dirname, "images", imgName);
+      document.getElementById("img-prev-div").setAttribute("src", img_url);
       // Call the func of kuw python script
       var options = {
-        scriptPath: path.join(__dirname, '../engine/'),
-        args: [imgname]
-      }
-      let pyshell = new PythonShell('kuw_filter.py', options);
-      pyshell.on('message', function (message) {
+        scriptPath: path.join(__dirname, "../engine/"),
+        args: [imgName],
+      };
+      let pyshell = new PythonShell("kuw_filter.py", options);
+      pyshell.on("message", function (message) {
         swal(message);
-      })
-
-
-
+      });
     }
 
-    async function show() {
-
-      after_filter_img_name = "after_kuwa_filter_" + imgname
-      let img_after_filter_url = path.join(__dirname, 'images', after_filter_img_name)
+    function show() {
+      var imgName = document.getElementById("imgname").value;
+      after_filter_img_name = "after_kuwa_filter_" + imgName;
+      let img_after_filter_url = path.join(
+        __dirname,
+        "images",
+        after_filter_img_name
+      );
+      console.log(img_after_filter_url);
 
       console.log("waiting for filter");
-      await filter();
-      document.getElementById('img-filter-div').setAttribute('src', img_after_filter_url)
+      document
+        .getElementById("img-filter-div")
+        .setAttribute("src", img_after_filter_url);
       document.getElementById("imgname").value = "";
     }
 
-    filter()
-    show()
-
-
+    filter();
+    show();
   }
 
   function filter_with_glg() {
     function filter() {
-
-      var imgname = document.getElementById("imgname").value
+      var imgName = document.getElementById("imgname").value;
+      // Show preview of origin img
+      let img_url = path.join(__dirname, "images", imgName);
+      document.getElementById("img-prev-div").setAttribute("src", img_url);
 
       // Call the func of kuw python script
       var options = {
-        scriptPath: path.join(__dirname, '../engine/'),
-        args: [imgname]
-      }
-      let pyshell = new PythonShell('FGLG.py', options);
-      pyshell.on('message', function (message) {
+        scriptPath: path.join(__dirname, "../engine/"),
+        args: [imgName],
+      };
+      let pyshell = new PythonShell("FGLG.py", options);
+      pyshell.on("message", function (message) {
         swal(message);
-      })
-
-
-
+      });
     }
 
-    async function show() {
-
-      after_filter_img_name = "after_glg_filter_" + imgname
-      let img_after_filter_url = path.join(__dirname, 'images', after_filter_img_name)
+    function show() {
+      var imgName = document.getElementById("imgname").value;
+      after_filter_img_name = "after_glg_filter_" + imgName;
+      let img_after_filter_url = path.join(
+        __dirname,
+        "images",
+        after_filter_img_name
+      );
 
       console.log("waiting for filter");
-      await filter();
-      document.getElementById('img-filter-div').setAttribute('src', img_after_filter_url)
+      document
+        .getElementById("img-filter-div")
+        .setAttribute("src", img_after_filter_url);
       document.getElementById("imgname").value = "";
     }
 
-    filter()
-    show()
+    filter();
+    show();
   }
 
   function filter_with_mean() {
     function filter() {
-
-      var imgname = document.getElementById("imgname").value
-
+      var imgName = document.getElementById("imgname").value;
+      // Show preview of origin img
+      let img_url = path.join(__dirname, "images", imgName);
+      document.getElementById("img-prev-div").setAttribute("src", img_url);
       // Call the func of kuw python script
       var options = {
-        scriptPath: path.join(__dirname, '../engine/'),
-        args: [imgname]
-      }
-      let pyshell = new PythonShell('mean_filter.py', options);
-      pyshell.on('message', function (message) {
+        scriptPath: path.join(__dirname, "../engine/"),
+        args: [imgName],
+      };
+      let pyshell = new PythonShell("mean_filter.py", options);
+      pyshell.on("message", function (message) {
         swal(message);
-      })
-
-
-
+      });
     }
 
-    async function show() {
-
-      after_filter_img_name = "after_glg_filter_" + imgname
-      let img_after_filter_url = path.join(__dirname, 'images', after_filter_img_name)
+    function show() {
+      var imgName = document.getElementById("imgname").value;
+      after_filter_img_name = "after_mean_filter_" + imgName;
+      let img_after_filter_url = path.join(
+        __dirname,
+        "images",
+        after_filter_img_name
+      );
 
       console.log("waiting for filter");
-      await filter();
-      document.getElementById('img-filter-div').setAttribute('src', img_after_filter_url)
+      document
+        .getElementById("img-filter-div")
+        .setAttribute("src", img_after_filter_url);
       document.getElementById("imgname").value = "";
     }
 
-    filter()
-    show()
+    filter();
+    show();
   }
 }
